56ea35578e514eee698546a6c9bd8efc8c836665
Heting.Zhao <heting.zhao@openjawtech.com>
2019-08-20 17:44:24 +0800

    First commit for spring cloud config server.

---
+++ src/main/java/com/example/zhtSpringCloudConfigServer/ZhtSpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhtSpringCloudConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhtSpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhtSpringCloudConfigServerApplication.class, args);
+	}
+
+}
